f4954e9e6b23105fcf815dcc0c77e87eee69bae1
Dinesh Bishwakarma <mylife.dinesh08@gmail.com>
2025-07-23 21:26:18 +0545

    static file

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,4 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
